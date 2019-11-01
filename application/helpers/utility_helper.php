78fbf3eb6e830cfe75594739cdbea4f6e20e5a9b
Taulant Sfishta <taulant.sfishta@gmail.com>
2019-11-01 17:42:31 +0100

    first commit2

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -1,11 +1,11 @@
 <?php
 
 function asset_url(){
-   return base_url().'assets/';
+   return 'https://thinkkosovo.cleverapps.io/assets/';
 }
 
 function upload_url(){
-   return base_url().'uploads/';
+   return 'https://thinkkosovo.cleverapps.io/uploads/';
 }
 
 ?>
