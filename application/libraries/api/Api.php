1bffd2ef706de32d5b561390d535bb36242751db
Taulant Sfishta <taulant.sfishta@gmail.com>
2019-11-01 17:13:32 +0100

    first commit2

--- application/libraries/api/Api.php
+++ application/libraries/api/Api.php
@@ -154,7 +154,7 @@ class Api {
       }else{
       $returnData[]= false;
     }
-    $returnData =[['one'=>$selectedData,'two'=>$selectedData_1]];
+    $returnData =[['one'=>$selectedData],['two'=>$selectedData_1]];
     return $returnData;
   }
 
